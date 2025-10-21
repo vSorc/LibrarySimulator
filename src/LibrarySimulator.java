513eb599351895dc96d22a7f47f9575dc2e0225f
khalifa <446104726@student.ksu.edu.sa>
2025-10-21 11:05:38 +0300

    intial commit

---
+++ src/LibrarySimulator.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+public class LibrarySimulator {
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to Library Simulator");
+
+
+
+
+    }
+}
