ef630e9b0caeadd127d7439297f778b0603f5884
khalifa <446104726@student.ksu.edu.sa>
2025-10-21 12:12:51 +0300

    Identification of variables used

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -1,14 +1,35 @@
 
+/*
+ * Project: Library Simulation System (Phase #1)
+ * Course: CSC 111 - Computer Programming - I
+ * Group Members:
+ * 1. Khalifa Al-Hashil , 446104726
+
+ */
 
 import java.util.Scanner;
+
 public class LibrarySimulator {
+
     public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+
+        int User_ID1 = 446123456 , User_Books1 = 0 ;
+        String User_Name1 = "Bader Al-Hashil" ;
+
+        int User_ID2 = 446012345 , User_Books2 = 0;
+        String User_Name2 = "Sultan Al-Khlaifi" ;
+
+        int User_ID3 = 446000000 , User_Books3 = 0;
+        String User_Name3 = "Mohammed Al-Subaie" ;
+
+        double totalRevenue = 0.0;
+        int TotalBorrows = 0;
+        int TotalReturns = 0;
+
+        int Choice;
 
 
-        System.out.println("Welcome to Library Simulator");
-
-
-
-
-    }
+        Input.close();
+}
 }
