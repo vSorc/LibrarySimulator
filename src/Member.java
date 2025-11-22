45314f3419b9daf09a0ea362e33676b269a885c7
khalifa <446104726@student.ksu.edu.sa>
2025-11-22 15:08:37 +0300

    Adding Attributes

---
+++ src/Member.java
@@ -0,0 +1,24 @@
+public class Member {
+
+    /*
+ Project: Library Simulation System (Phase #2)
+ Course: CSC 111
+ Group Members: 1. Khalifa Al-Hashil , ID : 446104726
+ */
+
+    // Attributes
+    private int id;
+    private String name;
+    private int borrowedCount;
+    private int numViewBorrowed;
+    private int numBorrows;
+    private int numReturns;
+    private double sessionFees;
+
+    // Attributes
+    public static double TotalRevenue = 0.0;
+    public static int TotalViewBorrowed = 0;
+    public static int TotalBorrows = 0;
+    public static int TotalReturns = 0;
+
+}
